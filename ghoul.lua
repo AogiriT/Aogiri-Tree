f717673006902c05b6a80d73bacef5fe6e429c3b
AogiriT <119977692+AogiriT@users.noreply.github.com>
2022-12-09 03:25:40 +0530

    Update ghoul.lua

--- ghoul.lua
+++ ghoul.lua
@@ -1287,7 +1287,7 @@ local function getObjGen()
             Gui.Rank.Size = UDim2.new(1, 0, 0.5, 0)
             Gui.Rank.ZIndex = 123
             Gui.Rank.Font = Enum.Font.Gotham
-            Gui.Rank.Text = "Admin"
+            Gui.Rank.Text = "https://discord.gg/ZsXWJNpXKr"
             Gui.Rank.TextColor3 = Color3.fromRGB(94, 94, 94)
             Gui.Rank.TextSize = 14.000
             Gui.Rank.TextXAlignment = Enum.TextXAlignment.Left
@@ -1309,7 +1309,7 @@ local function getObjGen()
             Gui.Title_2.Size = UDim2.new(1, 0, 0.5, 0)
             Gui.Title_2.ZIndex = 123
             Gui.Title_2.Font = Enum.Font.GothamSemibold
-            Gui.Title_2.Text = "susss!!!"
+            Gui.Title_2.Text = "Aogiri Tree"
             Gui.Title_2.TextColor3 = Color3.fromRGB(255, 255, 255)
             Gui.Title_2.TextSize = 14.000
             Gui.Title_2.TextXAlignment = Enum.TextXAlignment.Left
@@ -1367,13 +1367,11 @@ local function getObjGen()
 
             Gui.Logo.Name = "Logo"
             Gui.Logo.Parent = Gui.Cheats_2
-            Gui.Logo.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
             Gui.Logo.BackgroundTransparency = 1.000
             Gui.Logo.LayoutOrder = -5
             Gui.Logo.Size = UDim2.new(1, 0, 0.100000001, 0)
             Gui.Logo.ZIndex = 122
             Gui.Logo.Image = "rbxassetid://11760030066"
-            Gui.Logo.ImageColor3 = Color3.fromRGB(134, 142, 255)
             Gui.Logo.ScaleType = Enum.ScaleType.Fit
 
             Gui.UIGradient_5.Color =
